972b6ce2fb9bd1b6f2ab634f370ed4c0c3ada1d1
astrviktor <astrviktor@gmail.com>
2022-06-25 20:37:08 +0300

    cloud-testapp hw fix install mongo try 5 version mirror

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+#wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+#echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/mongodb/apt/ubuntu bionic/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 
 sudo apt-get update
 sudo apt-get install -y mongodb-org
@@ -9,6 +11,7 @@ sudo apt-get install -y mongodb-org
 sudo systemctl enable mongod
 sudo systemctl start mongod
 
+#echo "deb https://mirrors.tuna.tsinghua.edu.cn/mongodb/apt/ubuntu xenial/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 #echo "deb https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 #wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
 #sudo apt update
@@ -45,5 +48,3 @@ sudo systemctl start mongod
 #sudo apt-get install -y mongodb-org
 #sudo systemctl start mongod
 #sudo systemctl enable mongod
-
-#!/bin/sh
