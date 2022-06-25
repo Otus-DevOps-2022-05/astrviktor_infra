5af5db28feb1501f65d8e3384d0713e6d872ef47
astrviktor <astrviktor@gmail.com>
2022-06-25 17:49:08 +0300

    cloud-testapp hw fix install mongo

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sudo apt-get install -y mongodb
+sudo systemctl start mongodb
+sudo systemctl enable mongodb
+
 #sudo apt -y install docker docker.io
 #sudo systemctl enable docker
 #sudo systemctl start docker
@@ -7,9 +11,19 @@
 #  --name mongodb \
 #  --restart always \
 #mongo:4.2
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
+
+#sudo apt install -y git
+#sudo apt install -y openvpn
+#sudo apt install -y python
+#sudo apt install -y python2
+#wget https://raw.githubusercontent.com/ruped24/autovpn2/master/autovpn2.py
+#sudo mv autovpn2.py /usr/local/bin/autovpn2
+#sudo chmod +x /usr/local/bin/autovpn2
+#sudo /usr/local/bin/autovpn2 KR &
+#sleep 10
+#wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+#echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+#sudo apt-get update
+#sudo apt-get install -y mongodb-org
+#sudo systemctl start mongod
+#sudo systemctl enable mongod
