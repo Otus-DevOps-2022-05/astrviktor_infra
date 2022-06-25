b7c6a0132199bc8b8594360abe455b3ef2ba482d
astrviktor <astrviktor@gmail.com>
2022-06-25 15:01:14 +0300

    cloud-testapp hw fix mongo

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -2,8 +2,14 @@
 #sudo apt -y install docker docker.io
 #sudo systemctl enable docker
 #sudo systemctl start docker
-docker run --detach \
-  --publish 27017:27017 \
-  --name mongodb \
-  --restart always \
-mongo:4.2
+#sudo docker run --detach \
+#  --publish 27017:27017 \
+#  --name mongodb \
+#  --restart always \
+#mongo:4.2
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
