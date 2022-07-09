8c9f705eb024c87ed89a7781b02375105416484d
astrviktor <astrviktor@gmail.com>
2022-07-09 18:49:35 +0300

    terraform-2 hw done

---
+++ terraform/prod/main.tf
@@ -0,0 +1,33 @@
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+
+module "app" {
+  source          = "../modules/app"
+  public_key_path = var.public_key_path
+  app_disk_image  = var.app_disk_image
+  #  subnet_id       = var.subnet_id
+  subnet_id = yandex_vpc_subnet.app-subnet.id
+}
+
+module "db" {
+  source          = "../modules/db"
+  public_key_path = var.public_key_path
+  db_disk_image   = var.db_disk_image
+  #  subnet_id       = var.subnet_id
+  subnet_id = yandex_vpc_subnet.app-subnet.id
+}
+
+resource "yandex_vpc_network" "app-network" {
+  name = "app-network"
+}
+
+resource "yandex_vpc_subnet" "app-subnet" {
+  name           = "app-subnet"
+  zone           = "ru-central1-a"
+  network_id     = "${yandex_vpc_network.app-network.id}"
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
